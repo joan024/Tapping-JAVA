c2264518f0e9e318fcfc1ea311a14377f634b00b
joseprogramador1 <j.medina.1@insjoanbrudieu.cat>
2023-02-27 17:19:28 +0100

    Variable b resta

--- src/prova.java
+++ src/prova.java
@@ -4,8 +4,8 @@ public class prova {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=1;
-		int b;
-		System.out.println(a+a);
+		int b = 2;
+		System.out.println(a+a-b);
 	}
 
 }
